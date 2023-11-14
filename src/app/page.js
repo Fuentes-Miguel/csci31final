3be487262addccf9dba9725afb368dcc1c04ed7c
Michael Fuentes <michaelfuentes@Michaels-MacBook-Pro.local>
2023-11-13 21:16:09 -0800

    Add a card componet and improve styles

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,32 @@ import Image from "next/image";
 import Navbar from "./componets/Navbar";
 import PageTitle from "./componets/PageTitle";
 import PageContent from "./componets/PageContent";
+import Card from "./componets/Card";
+
 export default function Home() {
   return (
     <div>
       <Navbar />
       <div className="m-12">
         <PageTitle title="Home" />
+        <PageContent />
+        <div class="flex gap-6 w-full my-6 flex-wrap">
+          <Card
+            title="My first Card"
+            subtitle="My first subtitle"
+            description="My first description"
+          />
+          <Card
+            title="My second Card"
+            subtitle="My second subtitle"
+            description="My second description"
+          />
+          <Card
+            title="My third Card"
+            subtitle="My third subtitle"
+            description="My third description"
+          />
+        </div>
         <PageContent />
       </div>
     </div>
