cc1a9640c35352af2304c55a610b9dcdfac21d5a
Michael Fuentes <michaelfuentes@Michaels-MacBook-Pro.local>
2023-11-13 22:31:22 -0800

    Add supabase database integration

--- src/app/page.js
+++ src/app/page.js
@@ -3,30 +3,34 @@ import Navbar from "./componets/Navbar";
 import PageTitle from "./componets/PageTitle";
 import PageContent from "./componets/PageContent";
 import Card from "./componets/Card";
+import { createClient } from "@supabase/supabase-js";
 
-export default function Home() {
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  "https://cdkvactqpjevnsgsotmo.supabase.co",
+  process.env.SUPABASE_SECRET
+);
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const { data: cards, error } = await supabase.from("cards").select();
+
   return (
     <div>
       <Navbar />
       <div className="m-12">
         <PageTitle title="Home" />
         <PageContent />
-        <div class="flex gap-6 w-full my-6 flex-wrap">
-          <Card
-            title="My first Card"
-            subtitle="My first subtitle"
-            description="My first description"
-          />
-          <Card
-            title="My second Card"
-            subtitle="My second subtitle"
-            description="My second description"
-          />
-          <Card
-            title="My third Card"
-            subtitle="My third subtitle"
-            description="My third description"
-          />
+        <div className="flex gap-6 w-full my-6 flex-wrap">
+          {cards.map((card, idx) => (
+            <Card
+              key={idx}
+              title={card.title}
+              subtitle={card.subtitle}
+              description={card.description}
+            />
+          ))}
         </div>
         <PageContent />
       </div>
