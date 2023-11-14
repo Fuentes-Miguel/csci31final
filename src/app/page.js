653fb9bfacf6a83dd42f1e3e1c505118f1f6e4ce
Michael Fuentes <michaelfuentes@Michaels-MacBook-Pro.local>
2023-11-13 22:41:46 -0800

    update secret name

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import { createClient } from "@supabase/supabase-js";
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
   "https://cdkvactqpjevnsgsotmo.supabase.co",
-  process.env.SUPABASE_SECRET
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export const revalidate = 0;
